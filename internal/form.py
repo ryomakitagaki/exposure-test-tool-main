c65f53e5e744b51647887dc7222851eb984c6a91
horship <horship@hotmail.com>
2026-01-19 12:13:25 +0900

    インターフェイス変更

--- internal/form.py
+++ internal/form.py
@@ -20,16 +20,16 @@ def create_experiment_form():
 
     with st.form("experiment_form"):
         with tab1:
-            sample_name_1 = st.text_input("Sample Name", value="吹付PUF 23c", help="Required field", key="sample_name_1")
-            thickness_mm_1 = st.number_input("Thickness (mm)", min_value=0.0, value=24.93, step=0.1, key="thickness_mm_1")
-            initial_density_1 = st.number_input("Initial Density (kg/m³)", min_value=0.0, value=38.6044612611746, step=0.1,
-                                              key="initial_density_1")
-            temperature_1 = st.number_input("Temperature (°C)", value=23.0, step=0.1, key="temperature_1")
+            sample_name_1 = st.text_input("Sample Name", value="condition 01", help="Required field", key="sample_name_1")
+            thickness_mm_1 = st.number_input("Thickness (mm) ", help="Optional field",value=None, min_value=0.0, step=0.1, key="thickness_mm_1")
+            initial_density_1 = st.number_input("Initial Density (kg/m³) ", help="Optional field",value=None, min_value=0.0, step=0.1,
+                                                 key="initial_density_1")
+            temperature_1 = st.number_input("Temperature (°C)",  help="Required field", value=None, step=0.1, key="temperature_1")
             humidity_memo_1 = st.text_input("Humidity Notes", help="Optional field", key="humidity_memo_1")
 
             df = pd.DataFrame({
-                "測定日": pd.to_datetime(['2025-1-29', '2025-2-4', '2025-2-10', '2025-2-20', '2025-7-25']),
-                "熱伝導率": [0.021, 0.0217, 0.0222, 0.0228, 0.0264],
+                "測定日": pd.to_datetime(['','','','','']),
+                "熱伝導率": [None,None,None,None,None],
             })
             config = {
                 "測定日": st.column_config.DateColumn(
@@ -50,16 +50,16 @@ def create_experiment_form():
             )
 
         with tab2:
-            sample_name_2 = st.text_input("Sample Name",  value="吹付PUF 50c", help="Required field", key="sample_name_2")
-            thickness_mm_2 = st.number_input("Thickness (mm)", min_value=0.0, value=24.97, step=0.1, key="thickness_mm_2")
-            initial_density_2 = st.number_input("Initial Density (kg/m³)", min_value=0.0, value=38.8730455073759, step=0.1,
+            sample_name_2 = st.text_input("Sample Name", value="condition 02", help="Required field", key="sample_name_2")
+            thickness_mm_2 = st.number_input("Thickness (mm) ", help="Optional field", value=None, min_value=0.0, step=0.1, key="thickness_mm_2")
+            initial_density_2 = st.number_input("Initial Density (kg/m³) ",help="Optional field",value=None, min_value=0.0, step=0.1,
                                                 key="initial_density_2")
-            temperature_2 = st.number_input("Temperature (°C)", value=50.0, step=0.1, key="temperature_2")
+            temperature_2 = st.number_input("Temperature (°C)", help="Required field",value=None, step=0.1, key="temperature_2")
             humidity_memo_2 = st.text_input("Humidity Notes", help="Optional field", key="humidity_memo_2")
 
             df_2 = pd.DataFrame({
-                "測定日": pd.to_datetime(['2025-1-29', '2025-2-4', '2025-2-10', '2025-2-20', '2025-7-25']),
-                "熱伝導率": [0.0211, 0.0228, 0.0242, 0.025, 0.0273],
+                "測定日": pd.to_datetime(['','','','','']),
+                "熱伝導率": [None,None,None,None,None],
             })
             config = {
                 "測定日": st.column_config.DateColumn(
